422151c8f3f37bf104a53f629223223f138583b5
Albin Rönnkvist <albin.ronnkvist@tradera.com>
2023-12-28 19:57:42 +0100

    Add index constants

---
+++ Acme.Elasticsearch.Core/Constants/Indices/IndexConstantsBase.cs
@@ -0,0 +1,23 @@
+﻿namespace Acme.Elasticsearch.Core.Constants.Indices;
+
+public abstract class IndexConstantsBase
+{
+    protected abstract string BaseIndexName { get; }
+
+    private const string VersionSuffix = "-v";
+
+    public string GetTemplateName() 
+        => $"{BaseIndexName}-template";
+    
+    public string GetTemplatePattern() 
+        => $"{BaseIndexName}*";
+    
+    public string GetVersionedIndexName(int version) 
+        => $"{BaseIndexName}{VersionSuffix}{version}";
+
+    public string GetVersionedAlias(int version) 
+        => $"{BaseIndexName}{VersionSuffix}{version}";
+
+    public string GetSearchAlias() 
+        => BaseIndexName;
+}
---
+++ Acme.Elasticsearch.Core/Constants/Indices/ProductIndexConstants.cs
@@ -0,0 +1,6 @@
+﻿namespace Acme.Elasticsearch.Core.Constants.Indices;
+
+public class ProductIndexConstants : IndexConstantsBase
+{
+    protected override string BaseIndexName => "product";
+}
