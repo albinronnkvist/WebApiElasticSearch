40db495e33f513d7e22fc3da7260e6d6d14f6ec0
Albin Rönnkvist <albin.ronnkvist@tradera.com>
2024-01-22 18:29:55 +0100

    Delete IndexVersionRepository

--- Acme.Elasticsearch.ETL.Initializer/Repositories/Interfaces/IIndexVersionRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Acme.Elasticsearch.ETL.Initializer.Repositories;
-
-public interface IIndexVersionRepository
-{
-    public Task<int> GetLatestVersion();
-    public Task<int> GetLatestCompletedVersion();
-    public Task<int> CreateVersion();
-    public Task CompleteVersion(int indexVersion);
-}
